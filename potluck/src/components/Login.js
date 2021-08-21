8813299633ba2683a0a7ed42a2bd62b8f9301ed9
Rick <rmansfield@realpeoplesratings.com>
2021-08-21 15:31:47 -0500

    re-refactor Login.js again

--- potluck/src/components/Login.js
+++ potluck/src/components/Login.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 
 
 const Login = (props) => {
-    //2. Add whatever state necessary for form functioning.
     const [credentials, setCredentials] = useState({ username: "", password: "" })
-
-    // const error = ""; //replace with error state
     const [error, setError] = useState("");
 
     const handleChange = e => {
@@ -18,18 +15,14 @@ const Login = (props) => {
 
     const submit = e => {
         e.preventDefault();
-        //4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
         if (!credentials.username || !credentials.password) {
             return (setError("Username or Password not valid."));
         }
 
-        // make a post request to retrieve a token from the api
         axios.post('http://localhost:5000/api/login', credentials)
             .then(res => {
-                //5. If the username / password is equal to "Lambda" / "School", save that token to localStorage and redirect to a BubblePage route. 
                 localStorage.setItem("token", res.data.payload);
-                // when you have handled the token, navigate to the BubblePage route
-                props.history.push('/bubbles');
+                props.history.push('/potluck');
             })
             .catch(err => {
                 setError("Error logging in");
@@ -40,10 +33,7 @@ const Login = (props) => {
         <div>
             <h1>Welcome to Potluck Planner</h1>
             <div data-testid="loginForm" className="login-form">
-                {/* <h2>Build login form here</h2> */}
-                {/* //1. Build a form containing a username and password field. */}
                 <form onSubmit={submit}>
-                    {/* //6. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE id="username" and id="password" */}
                     <label >Username:</label>
                     <input
                         id="username"
@@ -64,23 +54,17 @@ const Login = (props) => {
                         onChange={handleChange}
                     />
                     <div>
-                        {/* //7. MAKE SURE YOUR SUBMIT BUTTON INCLUDES id="submit" */}
                         <button data-testid="submit"
                             id="submit" >Login
                         </button>
                     </div>
                 </form>
             </div>
-            {/* //8. MAKE SURE YOUR ERROR p tag contains the id="error" */}
             <p data-testid="errorMessage" id="error" className="error">{error}</p>
         </div>
     );
 };
 
 export default Login;
-
-//Task List:
-
-//3??? missing 
 
 
