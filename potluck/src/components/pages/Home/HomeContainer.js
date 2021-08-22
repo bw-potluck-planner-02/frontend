5f970c57be0b3b8efc71a575b04f53d2dfebf8a2
Rick <rmansfield@realpeoplesratings.com>
2021-08-22 11:35:33 -0500

    Refactor file structure to better accomodate flow

---
+++ potluck/src/components/pages/Home/HomeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import RenderHomePage from '.RenderHomePage';
+
+function HomeContainer ({ LoadingComponent }) {
+    return (
+        <>
+        <RenderHomePage />
+        </>
+    );
+}
+export default HomeContainer
