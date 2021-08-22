444b816b2d280e82e0c18b667b443525d5002280
Rick <rmansfield@realpeoplesratings.com>
2021-08-22 13:42:57 -0500

    Add UseApi, Homepage Useform to prep for Home page

---
+++ potluck/src/components/hooks/useAPI.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+export const useAPI = config => {
+  const [dataAPI, setData] = useState('');
+  const [error, setError] = useState('');
+  const { method, url, data } = config;
+
+  const moveData = () => {
+    console.log(config);
+    return axiosWithAuth()
+      [method](url, data)
+      .then(res => res.data)
+      .catch(err => {
+        console.log(err);
+        return err;
+      });
+  };
+  // console.log(dataAPI)
+  return [dataAPI, moveData, error];
+};
