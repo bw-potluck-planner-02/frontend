37ba98fdeb0def00da637440ecadd0b1884c9e20
Rick <rmansfield@realpeoplesratings.com>
2021-08-25 08:37:45 -0500

    fix private route with js

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import styled from "styled-components";
-import theme from "./Theme";
-import PrivateRoute from "./utils/PrivateRoute";
+// import theme from "./Theme";
+import PrivateRoute from "./utils/PrivateRoute.js";
 import Potluck from "./components/Potluck";
 import LoginPage from "./components/pages/Login/LoginContainer";
 import Registration from "./components/Registration";
