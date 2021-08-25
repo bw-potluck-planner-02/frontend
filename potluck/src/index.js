bf74c1003017c1bbf8f9df2add498eb7b267a56d
Rick <rmansfield@realpeoplesratings.com>
2021-08-25 08:26:16 -0500

    change case to upper on Theme

--- potluck/src/index.js
+++ potluck/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import theme from "./theme/index.js";
+import theme from "./Theme/index.js";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
