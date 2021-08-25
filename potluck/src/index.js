161e1bbe39d05f1a56ac9513b7c2b688de1d26b9
Rick <rmansfield@realpeoplesratings.com>
2021-08-25 08:29:40 -0500

    hardcode theme into index.js of /src

--- potluck/src/index.js
+++ potluck/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import theme from "./Theme/index.js";
+// import theme from "./Theme/index.js";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -11,6 +11,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { rootReducer } from "./Reducers";
 
+const theme = 
+ {
+
+  primaryColor: "#284B63",
+  secondaryColor: "#D9D9D9",
+  accentColor: "#3C6E71",
+  black: "#353535",
+  white: "FFFFFF",
+};
 // MOCKS+++++++++++++++++
 const { worker } = require("./mocks/browser.js");
 
