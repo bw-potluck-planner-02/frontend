9ce1b892c959f19034d2b3a8f4caa30ec3b323ea
Rick <ricksmycodeguy@gmail.com>
2021-11-23 19:45:15 -0600

    update broken team photos

--- potluck/src/components/Team.js
+++ potluck/src/components/Team.js
@@ -78,121 +78,115 @@ const StyledTeam = styled.div`
 
 export default function Team() {
     return (
-       <StyledTeam>
-           <div className='whole'>
-            <header>
-                <h1 id='hide'>POTLUCK PLANNER</h1>
-                <nav>
-                    <span className="navspans"><Link to="/">Home</Link></span>
-                    <span className="navspans"><Link to="/dashboard">DashBoard</Link></span>
-                    <span className="navspans"><Link to="/my-profile">My&nbsp;Profile</Link></span>
-                    <span className="navspans"><Link to="/meet-team">Meet&nbsp;our&nbsp;Team</Link></span>
-                </nav>
-            </header>
+        <StyledTeam>
+            <div className='whole'>
+                <header>
+                    <h1 id='hide'>POTLUCK PLANNER</h1>
+                    <nav>
+                        <span className="navspans"><Link to="/">Home</Link></span>
+                        <span className="navspans"><Link to="/dashboard">DashBoard</Link></span>
+                        <span className="navspans"><Link to="/my-profile">My&nbsp;Profile</Link></span>
+                        <span className="navspans"><Link to="/meet-team">Meet&nbsp;our&nbsp;Team</Link></span>
+                    </nav>
+                </header>
 
-            <div className='heading'>
-                <h1>Meet Our Team</h1>
-            </div>
-
-            <div className='main'>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://avatars.githubusercontent.com/u/86528475?v=4'
-                    alt='profile pic of tranequa'
-                    />
-                    <h2>TraNequa Fauntleroy</h2>
-                    <h3>Frontend UI Development</h3>
-                </div>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://media-exp1.licdn.com/dms/image/C5603AQFzQzwScHO3Sw/profile-displayphoto-shrink_800_800/0/1627430907751?e=1635379200&v=beta&t=GiahkmfDSOw0jf8bdeDndYgAr4_APWawgFJWi_7vS2Y'
-                    alt='profile pic for monica'
-                    />
-                    <h2>Monica Salas</h2>
-                    <h3>Frontend UI Development</h3>
-
-                </div>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://ca.slack-edge.com/ESZCHB482-U024A8UD2AE-816de42995b0-512'
-                    alt='profile of jonah'
-                    />
-                    <h2>Jonah Tillman</h2>
-                    <h3>Frontend UI Development</h3>
+                <div className='heading'>
+                    <h1>Meet Our Team</h1>
                 </div>
 
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://avatars.githubusercontent.com/u/16638790?v=4'
-                    alt='profile pic for rick'
-                    />
-                    <h2>Rick Mansfield</h2>
-                    <h3>Full Stack Developer | Fontend Team Lead</h3>
-                    {/* <h3>Connected Frontend UI and Backend Database</h3>
+                <div className='main'>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://avatars.githubusercontent.com/u/86528475?v=4'
+                            alt='profile pic of tranequa'
+                        />
+                        <h2>TraNequa Fauntleroy</h2>
+                        <h3>Frontend UI Development</h3>
+                    </div>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://ca.slack-edge.com/ESZCHB482-U0225RMJSEP-ec230f40698d-512'
+                            alt='profile pic for monica'
+                        />
+                        <h2>Monica Salas</h2>
+                        <h3>Frontend UI Development</h3>
+
+                    </div>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://ca.slack-edge.com/ESZCHB482-U024A8UD2AE-816de42995b0-512'
+                            alt='profile of jonah'
+                        />
+                        <h2>Jonah Tillman</h2>
+                        <h3>Frontend UI Development</h3>
+                    </div>
+
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://avatars.githubusercontent.com/u/16638790?v=4'
+                            alt='profile pic for rick'
+                        />
+                        <h2>Rick Mansfield</h2>
+                        <h3>Full Stack Developer | Fontend Team Lead</h3>
+                        {/* <h3>Connected Frontend UI and Backend Database</h3>
                     <h3>Create Base Organization/Repositories on GitHub</h3>
                     <h3>Set up initial React Project and File Structure</h3>
                     <h3>Create CRUD applications for functionality</h3> */}
-                </div>
+                    </div>
 
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://media-exp1.licdn.com/dms/image/C4E03AQF0DE28x4egLw/profile-displayphoto-shrink_400_400/0/1623124870817?e=1635379200&v=beta&t=xyU88yXd6iD3WRte4GqzgrgbUdQGTHuv_Ir9yymewkU'
-                    alt='profile-pic'
-                    />
-                    
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://ca.slack-edge.com/ESZCHB482-U021QR009N3-3544844f0026-512'
+                            alt='profile-pic'
+                        />
+                        <h2>Dharmik Savaliya</h2>
+                        <h3>Full Stack Developement coordination</h3>
 
-                    
-                    <h2>Dharmik Savaliya</h2>
-                    
-
-
-                    <h3>Full Stack Developement coordination</h3>
-
-                </div>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://picsum.photos/302'
-                    alt='profile-pic'
-                    />
-                    <h2>Derek Oh</h2>
-                    <h3>Frontend Developer</h3>
-                </div>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://avatars.githubusercontent.com/u/61172495?v=4'
-                    alt='profile pic for connor'
-                    />
-                    <h2>Connor Rayburn</h2>
-                    <h3>Backend Database Developement</h3>
-                </div>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://avatars.githubusercontent.com/u/77372140?v=4'
-                    alt='profile pic for amanuel'
-                    />
-                    <h2>Amanuel Gilay</h2>
-                    <h3>Backend Database Developement</h3>
-                </div>
-                <div className='card'>
-                    <img
-                    className='card-photo'
-                    src='https://avatars.githubusercontent.com/u/81659057?v=4'
-                    alt='profile pic for cara'
-                    />
-                    <h2>Cara Locke</h2>
-                    <h3>Full Stack Consultant</h3>
+                    </div>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://picsum.photos/302'
+                            alt='profile-pic'
+                        />
+                        <h2>Derek Oh</h2>
+                        <h3>Frontend Developer</h3>
+                    </div>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://avatars.githubusercontent.com/u/61172495?v=4'
+                            alt='profile pic for connor'
+                        />
+                        <h2>Connor Rayburn</h2>
+                        <h3>Backend Database Developement</h3>
+                    </div>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://avatars.githubusercontent.com/u/77372140?v=4'
+                            alt='profile pic for amanuel'
+                        />
+                        <h2>Amanuel Gilay</h2>
+                        <h3>Backend Database Developement</h3>
+                    </div>
+                    <div className='card'>
+                        <img
+                            className='card-photo'
+                            src='https://avatars.githubusercontent.com/u/81659057?v=4'
+                            alt='profile pic for cara'
+                        />
+                        <h2>Cara Locke</h2>
+                        <h3>Full Stack Consultant</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-       </StyledTeam> 
-        
+        </StyledTeam>
+
     )
 }
